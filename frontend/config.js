9eac914dc56d275361d6ccdc249e3884129e406a
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2026-04-20 19:22:29 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   ARCHITECTURE: "EC2",
-  API_BASE_URL: "http://YOUR_EC2_PUBLIC_DNS_OR_IP",
+  API_BASE_URL: "http://54.84.158.81",
   ALLOW_HTTP_API: true,
   APP_TITLE: "MusicCloud EC2"
 };
